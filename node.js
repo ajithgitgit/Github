208976c51cb9cfae98eaf09bc1506498a088e8b0
ajithgitgit <ajith.sdet.4@gmail.com>
2025-03-22 13:34:48 +0000

    ds

---
+++ node.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Sample Web Application!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
